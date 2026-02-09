ae625123bb6185683298c972db1062f690328287
Mofakkar Hossain <mofakkarhossain2001@gmail.com>
2026-02-10 01:19:32 +0600

    registered not found and error handler middleware in the main app file

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,38 @@
 import express, { Application } from "express";
+import cors from "cors";
+import { BookingRoutes } from "./modules/booking/booking.route";
+import { CategoryRoutes } from "./modules/category/category.route";
+import { ReviewRoutes } from "./modules/review/review.route";
+import { TutorRoutes } from "./modules/tutor/tutor.route";
+import { UserRoutes } from "./modules/user/user.route";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
-import cors from "cors";
-import { CategoryRoutes } from "./modules/category/category.route";
-import { TutorRoutes } from "./modules/tutor/tutor.route";
-import { BookingRoutes } from "./modules/booking/booking.route";
-import { ReviewRoutes } from "./modules/review/review.route";
-import { UserRoutes } from "./modules/user/user.route";
+import errorHandler from "./middlewares/globalErrorHandler";
+import { notFound } from "./middlewares/notFound";
 
 const app: Application = express();
 
+app.use(
+  cors({
+    origin: process.env.APP_URL || "http://localhost:3000",
+    credentials: true,
+  }),
+);
 app.use(express.json());
-app.use(cors());
 
-app.all("/api/auth/*splat", toNodeHandler(auth));
+app.all("/api/auth/*", toNodeHandler(auth));
 
-app.use("/api/categories", CategoryRoutes);
-
+app.use("/api/users", UserRoutes);
 app.use("/api/tutors", TutorRoutes);
-
+app.use("/api/categories", CategoryRoutes);
 app.use("/api/bookings", BookingRoutes);
-
 app.use("/api/reviews", ReviewRoutes);
 
-app.use("/api/admin/users", UserRoutes);
-
 app.get("/", (req, res) => {
-  res.send("SkillBridge API is running");
+  res.send("SkillBridge API is Running!");
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
